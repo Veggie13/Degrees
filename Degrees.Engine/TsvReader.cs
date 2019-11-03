5bc7edd5a585db43cbaf3b45e33fa9a1d3a88e00
Veggie13 <cmderochie+github@gmail.com>
2019-11-02 21:37:13 -0600

    Made loading faster by loading in blocks async while parsing the previous block.

--- Degrees.Engine/TsvReader.cs
+++ Degrees.Engine/TsvReader.cs
@@ -11,19 +11,46 @@ namespace Degrees.Engine
         public delegate T Parse<T>(IReadOnlyDictionary<string, int> schema, string[] fields);
         public static IEnumerable<T> ReadAll<T>(TextReader source, Parse<T> parser, Progress report = null)
         {
-            string line = source.ReadLine();
-            string[] fields = line.Split('\t');
+            if (report == null)
+            {
+                report = (lc) => { };
+            }
+
+            string headerLine = source.ReadLine();
+            string[] fields = headerLine.Split('\t');
             var schema = Parser.GetSchema(fields);
 
             int lineCount = 0;
-            line = source.ReadLine();
-            while (line != null)
+            var blockBuffer = new char[5120 * 1024];
+            int blockLength = source.ReadBlock(blockBuffer, 0, blockBuffer.Length);
+            string block = "";
+            while (0 < blockLength)
             {
-                fields = line.Split('\t');
-                yield return parser(schema, fields);
-                report(++lineCount);
-                line = source.ReadLine();
+                block += new string(blockBuffer, 0, blockLength);
+                var blockTask = source.ReadBlockAsync(blockBuffer, 0, blockBuffer.Length);
+                using (var reader = new StringReader(block))
+                {
+                    string line = reader.ReadLine();
+                    string nextLine;
+                    while ((nextLine = reader.ReadLine()) != null)
+                    {
+                        fields = line.Split('\t');
+                        yield return parser(schema, fields);
+                        ++lineCount;
+
+                        line = nextLine;
+                    }
+
+                    block = line;
+                }
+
+                report(lineCount);
+                blockTask.Wait();
+                blockLength = blockTask.Result;
             }
+
+            fields = block.Split('\t');
+            yield return parser(schema, fields);
         }
     }
 }
